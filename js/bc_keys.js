3f14a389046a29caf59f3c8b6a4291a52f9ec931
Elizanna <eahahn1s@capetigers.com>
2026-02-27 10:40:45 -0600

    first commit

--- js/bc_keys.js
+++ js/bc_keys.js
@@ -26,9 +26,29 @@
       characters with the _ character.
 
 */
+window.addEventListener("load", findKeyWords);
+window.addEventListener("load", makeKeyStyles);
+
+function findKeyWords() {
+   var articleElement = document.getElementById("doc");
+   var h2 = document.getElementsByTagName("h1")[0];
+
+   var asideElement = document.getElementById("aside");
+   asideElement.setAttribute("id", "keywords");
+
+var h1Element = document.createElement("h1");
+var h1Text = document.createTextNode("Keyword List");
 
 
 
+
+
+
+
+
+
+
+}
 
 
 
