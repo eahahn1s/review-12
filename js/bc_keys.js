1be70efeecdd4dd89b78d88165644149a70dc9a0
Elizanna <eahahn1s@capetigers.com>
2026-03-06 09:59:54 -0600

    second commit

--- js/bc_keys.js
+++ js/bc_keys.js
@@ -40,15 +40,88 @@ var h1Element = document.createElement("h1");
 var h1Text = document.createTextNode("Keyword List");
 
 
+ h1Element.appendChild(h1Text);
+  asideElement.appendChild(h1Element);
+  articleElement.insertBefore(asideElement, h2);
 
+ var orderedListElement = document.createElement("ol");
+  asideElement.appendChild(orderedListElement);
 
+ var keyWordElems = document.querySelectorAll("dfn");
+  var keyWords = [];
 
+for (var i = 0; i < keyWordElems.length; i++) {
+    keyWords.push(keyWordElems[i].textContent);
+ }
 
+keyWords.sort();
 
+keyWordElems.forEach((elem) => {
+    elem.setAttribute("id", "keyword_" + replaceWS(elem.textContent));
+  });
 
+for (var i = 0; i < keyWords.length; i++) {
+    var listItemElement = document.createElement("li");
+    var keyWordLink = document.createElement("a");
+    keyWordLink.innerHTML = keyWords[i];
 
-
+    keyWordLink.setAttribute("href", "#keyword_" + replaceWS(keyWords[i]));
+    listItemElement.appendChild(keyWordLink);
+    orderedListElement.appendChild(listItemElement);
+  }
 }
+
+function makeKeyStyles() {
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords { " +
+    "border: 3px solid rgb(101,101,101);" +
+    "float: right;" +
+    "margin: 20px 0px 20px 20px;" +
+    "padding: 10px;" +
+    "width: 320px;" +
+    "}"
+    , 0
+  );
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords h1 { " +
+    "font-size: 2em;" +
+    "margin: 5px;" +
+    "text-align: center;" +
+    "}"
+    , 1
+  );
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords ol { " +
+    "font-size: 1.2em;" +
+    "margin-left: 20px;" +
+    "}"
+    , 2
+  );
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords ol li { " +
+    "line-height: 1.5em;" +
+    "}"
+    , 3
+  );
+  document.styleSheets[document.styleSheets.length - 1].insertRule(
+    "aside#keywords ol li a { " +
+    "color: rgb(101,101,101);" +
+    "text-decoration: none;" +
+    "}"
+    , 4
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
